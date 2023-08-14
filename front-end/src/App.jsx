e6ad7b78ab7c899317546b4e8a8aeeb2e9685ca2
Eduardo Oliveira <eduuh.exe2.0@gmail.com>
2023-08-14 15:12:57 -0300

    estado do usuario

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [usuario, setUsuario] = useState("")
+
+  function handleUsuario(evento) {
+    setUsuario(evento.target.value)
+    console.log(usuario)
+  }
 
   return (
     <>
@@ -14,7 +20,12 @@ function App() {
     <form action="">
       <div>
         <label htmlFor="usuario">Usuário</label>
-        <input type="text" id= "usuario" placeholder='Digite seu usuario'/>
+        <input 
+        type="text" 
+        id= "usuario" 
+        placeholder='Digite seu usuario'
+        onChange={handleUsuario}
+        />
       </div>
       <div>
         <label htmlFor="senha">Senha</label>
